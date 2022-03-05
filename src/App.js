f348547ed31293c8b687f3fb8e9fd86c18ddf56e
Manuel Pando <pm.manu@gmail.com>
2022-03-05 19:08:59 +0000

    feat: new welcome message2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Manu
+          Learn React Manu Pando
         </a>
       </header>
     </div>
