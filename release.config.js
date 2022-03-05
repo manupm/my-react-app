ab1d76b772eabf40269823d4baf5c20fba66f048
Manuel Pando <pm.manu@gmail.com>
2022-03-05 18:58:40 +0000

    feat:feature welcome message

--- release.config.js
+++ release.config.js
@@ -2,9 +2,8 @@ module.exports = {
   branches: "master",
   repositoryUrl: "https://github.com/manupm/my-react-app",
   plugins: [
-    "@semantic-release/commit-analyzer", // analyse the commit messages and decid what our next version will be based on these commit messages
-    "@semantic-release/release-notes-generator", // generates release-notes for our release based on our commit messages descriptions
-    //'@semantic-release/npm', // useful to release an npm package
-    "@semantic-release/github",
-  ], // responsible for creating a hitub release
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ],
 };
