92c25ebf60d14a2588bf32683bbed3b76b3d3766
Manuel Pando <pm.manu@gmail.com>
2022-03-06 10:57:36 +0000

    adsfasdf

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Manu Pando Mones
+          Learn React Manu Pando
         </a>
       </header>
     </div>
